612996ac340d8e51edf33d0cb8f644941e487c19
Jhonatan Grymuza <jhonyzanatta@hotmail.com>
2021-01-21 10:45:31 -0300

    ajustes com CRUD REST

---
+++ src/main/java/com/br/SpringGradle/CarRepository.java
@@ -0,0 +1,11 @@
+package com.br.SpringGradle;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CarRepository extends JpaRepository<Car, Long> {
+	
+	//Car findByName(String name);
+
+}
--- src/main/java/com/br/SpringGradle/HelloWorld.java
+++ src/main/java/com/br/SpringGradle/HelloWorld.java
@@ -1,9 +1,16 @@
 package com.br.SpringGradle;
 
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
 
@@ -11,11 +18,19 @@ import org.springframework.web.context.request.WebRequest;
 @RequestMapping("/")
 public class HelloWorld {
 
+	
 	@GetMapping
 	public String sayHello() {
 		return "Hello from SpringBoot by Jhonatan";
 	}
-	
+
+	//exemplo de post
+	@PostMapping("/post")
+	public String sayHelloPost(@RequestBody Map<String, Object> payload) {
+		
+		return payload.get("name").toString();
+		
+	}
 	// sub path para o mesmo end - point com request paramento ?nome=jhonatan
 	@GetMapping("/subpath")
 	public String subPath(@RequestParam("name") String name) {
@@ -31,6 +46,8 @@ public class HelloWorld {
 	
 	// sub path para o mesmo end - point com request param paramento ?nome=jhonatan
 	@GetMapping("/requestParam")
+	@ResponseBody 
+	@ResponseStatus(code = HttpStatus.NO_CONTENT) // permite que eu consiga colocar um codigo customisado do meu resultado da requisição 
 	public String requestParam(@RequestParam(value="name",required = false) String name) {
 		return "This is one param of Endpoint/ " + name;
 	}
